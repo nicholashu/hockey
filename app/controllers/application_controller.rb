d1de16988dfc05ef8d9e815f762b8b861a900a32
Nick Hu <nick@nickhu.info>
2015-05-07 20:18:39 +0200

    migration fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+   def current_account
+    @iduser = session[:user_id]
+    @current_account ||= Account.find(@iduser) if @iduser
+  end
+  helper_method :current_account
 end
