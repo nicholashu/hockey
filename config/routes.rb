7cc1fae3e14ae173c0dd450227a14932ec6dbfcd
Nick Hu <nick@nickhu.info>
2015-05-07 18:00:58 +0200

    working

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   resources :listings
   resources :landing
+
   root 'landing#index'
+
+  get "/all_listings" => 'listings#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
