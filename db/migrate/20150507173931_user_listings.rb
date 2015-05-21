4fec1502b30cc600b770b190e8f6421b855f4240
Nick Hu <nick@nickhu.info>
2015-05-21 23:38:28 +0200

    heroku fixes

--- db/migrate/20150507173931_user_listings.rb
+++ db/migrate/20150507173931_user_listings.rb
@@ -2,7 +2,7 @@ class UserListings < ActiveRecord::Migration
   def change
   create_table :users
      add_column :users, :admin, :boolean, :default => false
-     add_column :users, :rating, :intiger
+     add_column :users, :rating, :integer
      add_column :users, :name, :string
   end
 end
