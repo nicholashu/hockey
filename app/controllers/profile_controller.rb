3e4901b2e39ec0720ee855f926f222f8cce6ef4a
Nick Hu <nick@nickhu.info>
2015-05-31 02:16:16 +0200

    my listings in profile

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -2,10 +2,13 @@ class ProfileController < ApplicationController
 
   def index
     current_account = @current_account
+   @listings = Listing.order("created_at desc")
   end
 
   def show
   	render :index
   end
+
+
 
 end
